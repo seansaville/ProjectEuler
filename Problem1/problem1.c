822299b57a047542ebe09c7d0aeabeb1cb731261
Sean Saville <sean.saville@hotmail.co.uk>
2014-11-16 21:21:52 +0000

    Made solution to Problem 1 more concise

--- Problem1/problem1.c
+++ Problem1/problem1.c
@@ -6,11 +6,7 @@ int main() {
 	int total = 0;
 
 	for (i = 1; i < num; i++) {
-		if (i % 15 == 0) {
-			total += i;
-		} else if (i % 3 == 0) {
-			total += i;
-		} else if (i % 5 == 0) {
+		if (i % 3 == 0 || i % 5 == 0) {
 			total += i;
 		}
 	}
